89224d44a485797d6c35acab3eb9b862bc99838e
pehtoorismies <pehtoorismies@gmail.com>
2019-09-02 11:47:47 +0300

    Add styled-component plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,8 @@ module.exports = {
           windows: false
         }
       }
-    }
+    },
+    `gatsby-plugin-styled-components`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
